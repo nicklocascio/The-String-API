e9793e60cf54241af8be4e44b28f1531938483f1
nicklocascio <nicklocascio@192.168.0.13>
2016-09-07 21:59:51 -0600

    Almost done with third one

--- String API/src/StringAPIChallenges.java
+++ String API/src/StringAPIChallenges.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class StringAPIChallenges
 	{
+	static int firstVowel;
+	public static int findFirstVowelIndex()
+		{
+		Scanner word3 = new Scanner(System.in);
+		System.out.println("Please input your full name.");
+		String fullName = word3.nextLine();
+		for(int i = 0; i < fullName.length(); i++)
+			{
+			if((fullName.substring(i, i + 1).equals("a")) || (fullName.substring(i, i + 1).equals("e")) || (fullName.substring(i, i + 1).equals("i")) || (fullName.substring(i, i + 1).equals("o")) || (fullName.substring(i, i + 1).equals("u")))
+				{
+				return i;
+				}
+			}
+		return firstVowel;
+		}
 	public static void main(String[] args)
 		{
 //		Scanner word = new Scanner(System.in);
@@ -10,49 +25,49 @@ public class StringAPIChallenges
 //		String result = (pluralWord.endsWith("s")) ? "That's plural!" : "That's not plural.";
 //		System.out.println(result);
 			
-		Scanner word2 = new Scanner(System.in);
-		System.out.println("Please input another word.");
-		String wordVowels = word2.nextLine();
-		int sum = 0;
-		for(int i=0; i<wordVowels.length(); i++)
-			{
-			switch(wordVowels.substring(i, i + 1))
-				{
-				case "a":
-					{
-					sum += i;		
-					break;
-					}
-				case "e":					
-					{					
-					sum += i;	
-					break;
-					}
-				case "i":
-					{
-					sum += i;	
-					break;
-					}
-				case "o":
-					{
-					sum += i;	
-					break;
-					}
-				case "u":
-					{
-					sum += i;	
-					break;
-					}
-					
-				}
-	
-			}
-		
-		System.out.println(sum);
+//		Scanner word2 = new Scanner(System.in);
+//		System.out.println("Please input another word.");
+//		String wordVowels = word2.nextLine();
+//		int sum = 0;
+//		for(int i=0; i<wordVowels.length(); i++)
+//			{
+//			switch(wordVowels.substring(i, i + 1))
+//				{
+//				case "a":
+//					{
+//					sum += i;		
+//					break;
+//					}
+//				case "e":					
+//					{					
+//					sum += i;	
+//					break;
+//					}
+//				case "i":
+//					{
+//					sum += i;	
+//					break;
+//					}
+//				case "o":
+//					{
+//					sum += i;	
+//					break;
+//					}
+//				case "u":
+//					{
+//					sum += i;	
+//					break;
+//					}					
+//				}	
+//			}		
+//		System.out.println(sum);
 //		Make method	
 //		Scanner word3 = new Scanner(System.in);
 //		System.out.println("Please input your full name.");
 //		String fullName = word3.nextLine();
+		findFirstVowelIndex();
+		
+		
 		
 
 		
